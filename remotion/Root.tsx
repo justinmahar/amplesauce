9f77d482d7dbb8a26a3e0c93996d1061f03c50a2
Justin Mahar <contact@justinmahar.com>
2025-10-02 17:19:02 -0400

    Fix amplesauce builds

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Composition } from 'remotion';
-import { MyComposition } from './Composition';
+import { AmpleSauceComposition } from './AmpleSauceComposition';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
-      <Composition id="Hello" component={MyComposition} durationInFrames={150} fps={30} width={1920} height={1080} />
+      <Composition
+        id="Hello"
+        component={AmpleSauceComposition}
+        durationInFrames={150}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
     </>
   );
 };
